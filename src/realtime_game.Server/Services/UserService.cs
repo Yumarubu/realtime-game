7dab467f99bec9505a4b0dfdeef0e320328e1b96
Yumarubu <241208@st.yoshida-g.ac.jp>
2025-11-28 13:16:32 +0900

    APIをこの調子で機能を追加&ちょっとだけ、ゲームの一番最初に登場させる注意書きシーンを編集

--- src/realtime_game.Server/Services/UserService.cs
+++ src/realtime_game.Server/Services/UserService.cs
@@ -1,6 +1,8 @@
 ﻿using MagicOnion;
 using MagicOnion.Server;
+using realtime_game.Server.Models.Contexts;
 using realtime_game.Shared.Interfaces.Services;
+using realtime_game.Shared.Models.Entities;
 using System.Linq.Expressions;
 
 namespace realtime_game.Server.Services
@@ -9,7 +11,23 @@ namespace realtime_game.Server.Services
     {
         public async UnaryResult<int> RegistUserAsync(string name)
         {
+            using var context = new GameDbContext();
+
+            //バリデーションチェック（名前登録済みかどうか）
+            if(context.Users.Count() > 0 && context.Users.Where(user => user.Name == name).Count() > 0)
+            {
+                throw new ReturnStatusException(Grpc.Core.StatusCode.InvalidArgument, "");
+            }
+
             //テーブルにレコードを追加
+            User user = new User();
+            user.Name = name;
+            user.Token = "";
+            user.Created_at = DateTime.Now;
+            user.Updated_at = DateTime.Now;
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return user.Id;
         }
     }
 }
