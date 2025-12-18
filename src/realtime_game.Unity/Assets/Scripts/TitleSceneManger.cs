f577d8ff5d781033f3ade1f7bdb68d6fe7eefba2
Yumarubu <241208@st.yoshida-g.ac.jp>
2025-12-18 09:59:16 +0900

    ゲームシーンに移動するための準備中...

---
+++ src/realtime_game.Unity/Assets/Scripts/TitleSceneManger.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManger
+{
+    void ChengeGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
